8b5c181ba9c4d5967bf6691f4bf0c51cdae800bf
Antonio Carlos Virissimo <antonio13050@gmail.com>
2023-08-18 18:49:03 -0700

    feat-getting the data from the form

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import { useState } from "react";
 
 function Login() {
-    return <div>Login</div>;
+    const { register, handleSubmit } = useForm();
+    const [user, setUser] = useState({});
+    const onSubmit = (data) => {
+        setUser(data);
+    };
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <input label="Email" {...register("email")} />
+            <input label="Senha" {...register("password")} />
+            <button type="submit">Logar</button>
+        </form>
+    );
 }
 
 export default Login;
