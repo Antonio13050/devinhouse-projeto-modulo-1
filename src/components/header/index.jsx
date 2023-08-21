afaf1cf98ccddbdeede0e6d903486abb8de87093
Antonio Carlos Virissimo <antonio13050@gmail.com>
2023-08-20 17:22:37 -0700

    feat-header style

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { UserContext } from "../../contexts/userContext";
+import { useContext } from "react";
+import "./styles.css";
+import logo from "../../assets/logo.png";
 
 function Header() {
+    const { setIsLogged } = useContext(UserContext);
+
+    const logout = () => {
+        setIsLogged(false);
+        localStorage.setItem("isLogged", false);
+    };
     const pages = [
         {
             route: "/",
@@ -18,11 +28,37 @@ function Header() {
     ];
     return (
         <header>
-            {pages.map((page, key) => (
-                <Link key={key} to={page.route}>
-                    {page.description}
-                </Link>
-            ))}
+            <nav class="navbar navbar-expand-lg bg-light px-3">
+                <div class="container-fluid">
+                    <a class="navbar-brand">
+                        <Link className="navbar-brand" to={"/"}>
+                            <img
+                                src={logo}
+                                alt="Logo"
+                                style={{ width: "200px" }}
+                            />
+                        </Link>
+                    </a>
+                    <div class="">
+                        <div class="navbar-nav">
+                            {pages.map((page, key) => (
+                                <a class="nav-link">
+                                    <Link
+                                        className="nav-link"
+                                        key={key}
+                                        to={page.route}
+                                    >
+                                        {page.description}
+                                    </Link>
+                                </a>
+                            ))}
+                        </div>
+                    </div>
+                    <button className="btn btn-danger" onClick={logout}>
+                        Sair
+                    </button>
+                </div>
+            </nav>
         </header>
     );
 }
--- src/pages/medications/index.jsx
+++ src/pages/medications/index.jsx
@@ -1,20 +1,11 @@
 import React from "react";
-import { UserContext } from "../../contexts/userContext";
-import { useContext } from "react";
 import Header from "../../components/header";
 
 function Medications() {
-    const { setIsLogged } = useContext(UserContext);
-
-    const logout = () => {
-        setIsLogged(false);
-        localStorage.setItem("isLogged", false);
-    };
     return (
         <div>
             <Header />
             Medications
-            <button onClick={logout}>Sair</button>
         </div>
     );
 }
