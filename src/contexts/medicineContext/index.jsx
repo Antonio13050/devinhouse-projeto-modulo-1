c182b9831ec3591a397130cd5d2c5ad93f248be4
Antonio Carlos Virissimo <antonio13050@gmail.com>
2024-04-24 21:22:10 -0300

    feat: Added initial data

--- src/contexts/medicineContext/index.jsx
+++ src/contexts/medicineContext/index.jsx
@@ -10,23 +10,40 @@ const ListMedicineProvider = ({ children }) => {
                   {
                       id: 1,
                       name: "Paracetamol",
-                      laboratory: "Laboratório 1",
+                      laboratory: "Laboratório 1",
                       dosage: "Dosagem 1",
                       type: "Tipo 1",
                       price: "3,99",
                       description:
-                          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.1",
+                          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
                   },
                   {
                       id: 2,
                       name: "Ibuprofeno",
-                      laboratory: "Laboratório 2",
+                      laboratory: "Laboratório 2",
                       dosage: "Dosagem 2",
                       type: "Tipo 2",
                       price: "4,99",
-
                       description:
                           "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+                  },
+                  {
+                      id: 3,
+                      name: "Aspirina",
+                      laboratory: "Laboratório 3",
+                      dosage: "Dosagem 3",
+                      type: "Tipo 3",
+                      price: "2,50",
+                      description: "Descrição do medicamento Aspirina.",
+                  },
+                  {
+                      id: 4,
+                      name: "Dipirona",
+                      laboratory: "Laboratório 4",
+                      dosage: "Dosagem 4",
+                      type: "Tipo 4",
+                      price: "3,75",
+                      description: "Descrição do medicamento Dipirona.",
                   },
               ]
     );
